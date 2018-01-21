e4eacc501cf1e6e0d5748375540fd8de55a1e72d
Soraph <soraph88@gmail.com>
2018-01-21 11:08:22 +0100

    Created base server and model configuration.

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+api = Api(app)
---
+++ models.py
@@ -0,0 +1,5 @@
+# from app import db
+
+# Refer to http://flask-sqlalchemy.pocoo.org/2.3/models/
+# Class Table_name(db.Model):
+#   pass
